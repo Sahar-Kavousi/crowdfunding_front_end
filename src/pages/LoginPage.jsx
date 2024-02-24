b9853a709a1bf1d8d16d68bb376e8936fcf28cf6
Sahar Kavousi <s.ka1366@gmail.com>
2024-02-24 10:36:01 +0800

    added the redirects file in public

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
-// import { AiOutlineGooglePlus } from "react-icons/ai";
 function LoginPage() {
   return (
     <div className="p-4">
       <LoginForm />
-      <div className="m-4 bg-blue-400 border-spacing-4">
-        {/* <AiOutlineGooglePlus className="flex text-red-400 text-lg" /> */}
-      </div>
+      <div></div>
       <p>
         If you don't have user, Signup
         <Link
